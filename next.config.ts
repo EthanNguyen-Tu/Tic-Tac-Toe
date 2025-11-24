07e69344e485c24670d594cd382e2fc221206237
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-11-23 19:58:38 -0500

    chore: add GitHub Pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/Tic-Tac-Toe",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
+
